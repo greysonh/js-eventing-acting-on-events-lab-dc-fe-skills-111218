8c41a97fc0b3bcb6b37302642b375441e5cc0fda
Greyson Harris <gharris@esri.com>
2019-01-09 02:56:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,6 @@
 const dodger = document.getElementById("dodger");
 dodger.style.backgroundColor = "#ff5bab";
 
-document.addEventListener("keydown", function(e) {
-  if (e.which === 37) {
-    var leftNumbers = dodger.style.left.replace("px","");
-    var left = parseInt(leftNumbers, 10);
-    dodger.style.left = `${left - 1}px`;
-  }
-})
-
 function moveDodgerLeft() {
   var leftNumbers = dodger.style.left.replace('px', '');
   var left = parseInt(leftNumbers, 10);
@@ -17,3 +9,12 @@ function moveDodgerLeft() {
     dodger.style.left = `${left - 1}px`
   }
 }
+
+document.addEventListener("keydown", function(e) {
+  if (e.which === 37) {
+    var leftNumbers = dodger.style.left.replace("px","");
+    var left = parseInt(leftNumbers, 10);
+    dodger.style.left = `${left - 1}px`;
+  }
+})
+
