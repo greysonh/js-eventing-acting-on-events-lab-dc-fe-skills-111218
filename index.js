712a6a0f4d780201ae28b788d8fb6c34c4590a9f
Greyson Harris <gharris@esri.com>
2019-01-09 02:46:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ document.addEventListener('keydown', function(e) {
   if (e.which === 37) {
     var leftNumbers = dodger.style.left.replace("px","")
     var left = parseInt(leftNumbers, 10)
-    dodger.style.left = "${left - 1}px"
+    dodger.style.left = '${left - 1}px'
   }
 })
