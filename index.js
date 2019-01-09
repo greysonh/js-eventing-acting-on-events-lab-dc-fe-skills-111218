88ea4d1e892e00879089da2b497f9349e6075eba
Greyson Harris <gharris@esri.com>
2019-01-09 03:19:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ function moveDodgerLeft() {
 
 function moveDodgerRight() {
   var leftNumbers = dodger.style.left.replace('px', '');
-  var right = parseInt(leftNumbers, 10);
+  var left = parseInt(leftNumbers, 10);
  
-  if (right > 0) {
-    dodger.style.left = `${right + 2}px`
+  if (left > 0) {
+    dodger.style.left = `${left + 2}px`
   }
 }
 
