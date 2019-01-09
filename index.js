7c297d1668118f428f55691e5e3856c052bc321a
Greyson Harris <gharris@esri.com>
2019-01-09 02:45:50 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const dodger = document.getElementById("dodger");
 dodger.style.backgroundColor = "#ff5bab";
 
-document.addEventListener('keydown', function (e) {
+document.addEventListener('keydown', function(e) {
   if (e.which === 37) {
     var leftNumbers = dodger.style.left.replace("px","")
     var left = parseInt(leftNumbers, 10)
