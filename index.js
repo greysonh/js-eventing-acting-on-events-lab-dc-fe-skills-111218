eda647193970b1dc901449c6679eb962a47e940f
Greyson Harris <gharris@esri.com>
2019-01-09 03:17:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,7 +10,14 @@ function moveDodgerLeft() {
   }
 }
 
-
+function moveDodgerLeft() {
+  var leftNumbers = dodger.style.left.replace('px', '');
+  var right = parseInt(leftNumbers, 10);
+ 
+  if (right > 0) {
+    dodger.style.left = `${left - 2}px`
+  }
+}
 
 document.addEventListener("keydown", function(e) {
   if (e.which === 37) {
