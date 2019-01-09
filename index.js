be2b4ad99a9e93f868b08da74da34035d9cd8ea0
Greyson Harris <gharris@esri.com>
2019-01-09 03:22:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function moveDodgerLeft() {
   var left = parseInt(leftNumbers, 10);
  
   if (left > 0) {
-    dodger.style.left = `${left - 2}px`
+    dodger.style.left = `${left - 5}px`
   }
 }
 
@@ -15,7 +15,7 @@ function moveDodgerRight() {
   var left = parseInt(leftNumbers, 10);
  
   if (left < 360) {
-    dodger.style.left = `${left + 2}px`
+    dodger.style.left = `${left + 5}px`
   }
 }
 
